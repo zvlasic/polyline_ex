d9d7521047e0db88eeba00e604cdd71a4828b8ff
Zlatko Vlasic <zlayo1@gmail.com>
2025-12-18 09:22:41 +0100

    Update deps, remove encode, change decode

--- lib/polyline.ex
+++ lib/polyline.ex
@@ -8,11 +8,8 @@ defmodule Polyline do
   The default is `5`, which correlates to approximately 1 meter of precision.
 
   ## Examples
-      iex> Polyline.encode([{-120.2, 38.5}, {-120.95, 40.7}, {-126.453, 43.252}])
-      "_p~iF~ps|U_ulLnnqC_mqNvxq`@"
-
       iex> Polyline.decode("_p~iF~ps|U_ulLnnqC_mqNvxq`@")
-      [{-120.2, 38.5}, {-120.95, 40.7}, {-126.453, 43.252}]
+      [[-120.2, 38.5], [-120.95, 40.7], [-126.453, 43.252]]
   """
 
   import Bitwise
@@ -20,64 +17,14 @@ defmodule Polyline do
   @default_precision 5
 
   @doc ~S"""
-  Encode a List of coordinate tuples into a Polyline String. Also works with
-  `Geo.LineString` structs (see https://hex.pm/packages/geo).
-
-  ## Examples
-      iex> Polyline.encode([{-120.2, 38.5}, {-120.95, 40.7}, {-126.453, 43.252}])
-      "_p~iF~ps|U_ulLnnqC_mqNvxq`@"
-
-      iex> Polyline.encode([{-120.2, 38.5}, {-120.95, 40.7}, {-126.453, 43.252}], 6)
-      "_izlhA~rlgdF_{geC~ywl@_kwzCn`{nI"
-
-      iex> "LINESTRING(-120.2 38.5, -120.95 40.7, -126.453 43.252)"
-      ...> |> Geo.WKT.decode!
-      ...> |> Map.get(:coordinates)
-      ...> |> Polyline.encode
-      "_p~iF~ps|U_ulLnnqC_mqNvxq`@"
-  """
-  def encode(coordinates, precision \\ @default_precision) do
-    factor = :math.pow(10, precision)
-
-    rounded_coordinates =
-      Enum.map(coordinates, fn {x, y} ->
-        {round(x * factor), round(y * factor)}
-      end)
-
-    elem(do_encode(rounded_coordinates), 0)
-  end
-
-  defp do_encode([]), do: {"", nil}
-
-  defp do_encode([{x0, y0} | coordinates]) do
-    Enum.reduce(coordinates, encode_step({x0, y0}, {"", {0, 0}}), fn t, acc ->
-      encode_step(t, acc)
-    end)
-  end
-
-  defp encode_step({x, y}, {acc, {x_prev, y_prev}}) do
-    {acc <> encode_int(y - y_prev) <> encode_int(x - x_prev), {x, y}}
-  end
-
-  defp encode_int(x) do
-    (x <<< 1)
-    |> unsign()
-    |> collect_chars()
-    |> to_string()
-  end
-
-  defp collect_chars(c) when c < 0x20, do: [c + 63]
-  defp collect_chars(c), do: [(0x20 ||| (c &&& 0x1F)) + 63 | collect_chars(c >>> 5)]
-
-  @doc ~S"""
-  Decode a polyline String into a List of `{lon, lat}` tuples.
+  Decode a polyline String into a List of `[lon, lat]` lists.
 
   ## Examples
       iex> Polyline.decode("_p~iF~ps|U_ulLnnqC_mqNvxq`@")
-      [{-120.2, 38.5}, {-120.95, 40.7}, {-126.453, 43.252}]
+      [[-120.2, 38.5], [-120.95, 40.7], [-126.453, 43.252]]
 
       iex> Polyline.decode("_izlhA~rlgdF_{geC~ywl@_kwzCn`{nI", 6)
-      [{-120.2, 38.5}, {-120.95, 40.7}, {-126.453, 43.252}]
+      [[-120.2, 38.5], [-120.95, 40.7], [-126.453, 43.252]]
   """
   def decode(str, precision \\ @default_precision)
   def decode(str, _) when str == "", do: []
@@ -88,7 +35,7 @@ defmodule Polyline do
     str
     |> String.to_charlist()
     |> decode_list([{0, 0}], nil)
-    |> Enum.reduce([], fn {x, y}, acc -> [{x / factor, y / factor} | acc] end)
+    |> Enum.reduce([], fn {x, y}, acc -> [[x / factor, y / factor] | acc] end)
     |> tl()
   end
 
@@ -118,8 +65,6 @@ defmodule Polyline do
 
   defp decode_char(char, shift), do: (char - 63 &&& 0x1F) <<< shift
 
-  defp unsign(x) when x < 0, do: -(x + 1)
-  defp unsign(x), do: x
   defp sign(result) when (result &&& 1) === 1, do: -((result >>> 1) + 1)
   defp sign(result), do: result >>> 1
 end
--- test/polyline_test.exs
+++ test/polyline_test.exs
@@ -4,31 +4,14 @@ defmodule PolylineTest do
 
   doctest Polyline
 
-  @example [{-120.2, 38.5}, {-120.95, 40.7}, {-126.453, 43.252}]
-  @example_slashes [{-82.55, 35.6}, {-82.55015, 35.59985}, {-82.55, 35.6}]
-
-  test "encode an empty List" do
-    assert Polyline.encode([]) == ""
-  end
-
-  test "encode a single location" do
-    assert Polyline.encode([{-120.2, 38.5}]) == "_p~iF~ps|U"
-  end
-
-  test "encode a List of lon/lat pairs into an String" do
-    assert Polyline.encode(@example) == "_p~iF~ps|U_ulLnnqC_mqNvxq`@"
-  end
-
-  test "encode a List of lon/lat pairs into an String with custom precision" do
-    assert Polyline.encode(@example, 6) == "_izlhA~rlgdF_{geC~ywl@_kwzCn`{nI"
-  end
+  @example [[-120.2, 38.5], [-120.95, 40.7], [-126.453, 43.252]]
 
   test "decode an empty List" do
     assert Polyline.decode("") == []
   end
 
   test "decode a single location" do
-    assert Polyline.decode("_p~iF~ps|U") == [{-120.2, 38.5}]
+    assert Polyline.decode("_p~iF~ps|U") == [[-120.2, 38.5]]
   end
 
   test "decode a String into a List of lon/lat pairs" do
@@ -39,103 +22,10 @@ defmodule PolylineTest do
     assert Polyline.decode("_izlhA~rlgdF_{geC~ywl@_kwzCn`{nI", 6) == @example
   end
 
-  test "encode -> decode" do
-    assert @example_slashes |> Polyline.encode() |> Polyline.decode() == @example_slashes
-  end
-
-  test "decode -> encode" do
-    assert "_chxEn`zvN\\\\]]" |> Polyline.decode() |> Polyline.encode() == "_chxEn`zvN\\\\]]"
-  end
-
-  test "decode a long string to Geometry" do
-    res =
-      [".", "test", "fixtures", "long.polyline.txt"]
-      |> Path.join()
-      |> File.read!()
-      |> String.trim()
-      |> Polyline.decode()
-
-    expected =
-      [".", "test", "fixtures", "long.geo.json"]
-      |> Path.join()
-      |> File.read!()
-      |> String.trim()
-
-    assert %Geo.LineString{coordinates: res} |> Geo.JSON.encode!() |> Poison.encode!() == expected
-  end
-
-  test "encode an over-precise string same way as reference implementation" do
-    # reference value is generated using following JS google maps API code:
-    # raw_coords = [[-87.650934, 41.875332], [-87.650938, 41.875336], [-87.650941, 41.87534]]
-    # path = raw_coords.map(function(coords){ return new google.maps.LatLng(coords[1], coords[0]) })
-    # google.maps.geometry.encoding.encodePath(path)
-    assert Polyline.encode([
-             {-87.650933, 41.875332},
-             {-87.650936, 41.875336},
-             {-87.650942, 41.875340}
-           ]) == "ywq~Fhi~uOA@??"
-  end
-
-  test "encode a long string" do
-    res =
-      [".", "test", "fixtures", "long.geo.json"]
-      |> Path.join()
-      |> File.read!()
-      |> Poison.decode!()
-      |> Geo.JSON.decode!()
-      |> Map.get(:coordinates)
-      |> Polyline.encode()
-
-    expected =
-      [".", "test", "fixtures", "long.polyline.txt"]
-      |> Path.join()
-      |> File.read!()
-      |> String.trim()
-
-    assert res == expected
-  end
-
-  test "identity with a long string to Geometry" do
-    polyline =
-      [".", "test", "fixtures", "long.polyline.txt"]
-      |> Path.join()
-      |> File.read!()
-      |> String.trim()
-
-    assert polyline |> Polyline.decode() |> Polyline.encode() == polyline
-  end
-
-  test "identity with 0,0 point" do
-    polyline = "??"
-
-    assert polyline |> Polyline.decode() |> Polyline.encode() == polyline
-  end
-
   test "discard leftover elements when decoding" do
     string =
       "i|~wAeo{aVw@i@SI]EkN^c@@KfXGNULcCo@}HgByEkAcFcAsCk@oAYeAYgZuGiBu@wCi@iGo@eKBiHx@aGzAeMpEgJ`Dy@wC~@kK|D_A`@yLlEkAXuJhDuAj@yAp@mKzD{h@bRu@NcIpCmIbDmGxBk@RkD`AgBj@wAf@a@mBe@sCiCiNkCcMgCkMeBZWE}@BmKsAkCWwE]{BGyC?iBD}BJwCVgDb@mByNu@wSGaC{DL"
 
     assert string |> Polyline.decode() |> Enum.count() == 64
-  end
-
-  property "encoding/decoding returns approximately the same points" do
-    check all(points <- point_list()) do
-      encoded = Polyline.encode(points)
-      decoded = Polyline.decode(encoded)
-      assert length(decoded) == length(points)
-
-      for {expected, actual} <- Enum.zip(points, decoded) do
-        assert_approx_equal_points(expected, actual)
-      end
-    end
-  end
-
-  defp coord, do: float(min: -180.0, max: 180.0)
-  defp point, do: tuple({coord(), coord()})
-  defp point_list, do: nonempty(list_of(point(), max_length: 10))
-
-  defp assert_approx_equal_points({expected_lon, expected_lat}, {actual_lon, actual_lat}) do
-    assert_in_delta expected_lon, actual_lon, 0.00001
-    assert_in_delta expected_lat, actual_lat, 0.00001
   end
 end
